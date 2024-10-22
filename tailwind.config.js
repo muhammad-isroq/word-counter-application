f53f277a49c2efe2746d0d609fd425c42fb5588b
muhammad-isroq <muhammadisroq7@gmail.com>
2024-10-22 17:09:04 +0700

    aplikasi penghitung kata secara real time

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './*.html', // Mengarahkan ke semua file HTML di folder yang sama
+    './*.css',  // Mengarahkan ke semua file CSS di folder yang sama
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
